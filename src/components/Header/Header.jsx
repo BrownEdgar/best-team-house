11e95c78592907cc091467d3d53db6cbefd81a6e
Rubo001 <ruben.budaghyan@icloud.com>
2024-02-16 21:10:39 +0400

    Header is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,41 @@
+import { addUser } from '../../features/headerSlice'
+import FormButton from '../FormButton/FormButton'
 import Navigation from '../Navigation/Navigation'
 import './Header.scss'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 export default function Header() {
-	const header = useSelector(state => state.header)
-
-	return (
-		<div className="HeaderBG">
-			<div className="container Header">
-				<div className="Header__left">
-					<Navigation />
-					<div className="Header__content">
-						<h3>{header.slogan}</h3>
-						<h1>{header.title}</h1>
-						<p>{header.description}</p>
-					</div>
-					<div className="Header__form">
-						<form>
-							<input type="email" name="email" placeholder='Enter your email... ' />
-							<button>Get Started</button>
-						</form>
-					</div>
-				</div>
-				<div className="Header__right">
-					<div className="Header__buttons">
-						<button className='signin'>Sign In</button>
-						<button className='register'>Register</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  const header = useSelector(state => state.header)
+  const dispatch = useDispatch();
+  const addUserForm = (e) => {
+    e.preventDefault();
+    const { email } = e.target
+    dispatch(addUser({ email: email.value }))
+    e.target.reset();
+  }
+  return (
+    <div className="HeaderBG">
+      <div className="container Header">
+        <div className="Header__left">
+          <Navigation />
+          <div className="Header__content">
+            <h3>{header.slogan}</h3>
+            <h1>{header.title}</h1>
+            <p>{header.description}</p>
+          </div>
+          <div className="Header__form">
+            <form onSubmit={addUserForm}>
+              <input type="email" name="email" placeholder='Enter your email... ' />
+              <FormButton text={"Get Started"} />
+            </form>
+          </div>
+        </div>
+        <div className="Header__right">
+          <div className="Header__buttons">
+            <button className='signin'>Sign In</button>
+            <button className='register'>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/features/headerSlice.js
+++ src/features/headerSlice.js
@@ -1,12 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialStateValue = {
-    slogan:'easy way to find your property',
-    title:'find the perfect place to stay with your family',
-    description:'Enjoy a stress-free and enjoyable vacation with your loved ones. Start your search today and find your dream property!'
+    slogan: 'easy way to find your property',
+    title: 'find the perfect place to stay with your family',
+    description: 'Enjoy a stress-free and enjoyable vacation with your loved ones. Start your search today and find your dream property!',
+    users:[]
 }
 const headerSlice = createSlice({
-    name:'header',
+    name: 'header',
     initialState: initialStateValue,
-    reducers:{}
+    reducers: {
+        addUser(state, action) {
+            state.users.push(action.payload);
+        }
+    }
 })
 export default headerSlice.reducer;
+export const {addUser} = headerSlice.actions;
